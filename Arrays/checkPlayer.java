17c832231fe9c98188c98095f16bf37e782cb9ff
Piyush <piyushpandita151@gmail.com>
2022-09-05 15:25:56 +0530

    Renaming the codechef Question

--- Arrays/checkPlayer.java
+++ Arrays/checkPlayer.java
@@ -1,8 +1,5 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
