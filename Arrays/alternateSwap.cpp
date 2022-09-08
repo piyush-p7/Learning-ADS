29aaf027f8472d01a50fa25a76b825d9591ffb65
Piyush <piyushpandita151@gmail.com>
2022-09-08 11:11:38 +0530

    Swapping the indexes alternatively

--- Arrays/alternateSwap.cpp
+++ Arrays/alternateSwap.cpp
@@ -11,6 +11,9 @@ int getValues(int arr[],int size){
 int swapAlter(int arr[],int size){
     cout<<"Alternately Swapped array: ";
     for(int i=0;i<size;i+=2){
+        if(i+1==size){
+            break;
+        }
         swap(arr[i],arr[i+1]);
     }
     for(int i=0;i<size;i++){
