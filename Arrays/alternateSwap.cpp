873a8564657c35b8f20c6d02f0f4cedeb1642d59
Piyush <piyushpandita151@gmail.com>
2022-09-09 11:29:00 +0530

    Swapping the alternate values in the array

--- Arrays/alternateSwap.cpp
+++ Arrays/alternateSwap.cpp
@@ -11,10 +11,9 @@ int getValues(int arr[],int size){
 int swapAlter(int arr[],int size){
     cout<<"Alternately Swapped array: ";
     for(int i=0;i<size;i+=2){
-        if(i+1==size){
-            break;
+        if(i+1<size){
+            swap(arr[i],arr[i+1]);
         }
-        swap(arr[i],arr[i+1]);
     }
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
