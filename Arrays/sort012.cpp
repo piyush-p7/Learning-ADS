6c7ae3a746e3048eae9cacf25da423b8e0a8f2f0
Piyush <piyushpandita151@gmail.com>
2022-09-12 00:20:33 +0530

    Sorting 0, 1, 2.

---
+++ Arrays/sort012.cpp
@@ -0,0 +1,23 @@
+// SORT 0-1-2
+
+// Dutch National Flag problem
+
+#include<iostream>
+#include<array>
+using namespace std;
+
+// void swap(int a,int b){
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+void sort012(array<int,10>arr,int size){
+    
+    
+}
+int main(){
+    array<int,10> arr = {1,0,0,2,1,1,0,0,2,1};
+    sort012(arr,10);
+
+}
