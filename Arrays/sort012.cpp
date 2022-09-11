57393a367cc54ee8e9a95971659caea3717026e3
Piyush Pandita <96694286+piyush-p7@users.noreply.github.com>
2022-09-12 00:28:06 +0530

    Update sort012.cpp

--- Arrays/sort012.cpp
+++ Arrays/sort012.cpp
@@ -13,7 +13,23 @@ using namespace std;
 // }
 
 void sort012(array<int,10>arr,int size){
-    
+    int low = 0;
+    int high = size-1;
+    int mid = 0;
+    while(mid<=high){
+        switch(arr[mid]){
+            case 0:
+                  swap(arr[low++],arr[mid++]);
+            case 1:
+                  mid++;
+            case 2:
+                swap(arr[high--],arr[mid]);
+        }
+    }
+    //Printing the values.
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
     
 }
 int main(){
