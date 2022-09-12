8838a83be1ec9df82c33307d8851cecde0753564
Piyush <piyushpandita151@gmail.com>
2022-09-12 22:16:58 +0530

    Arrays Both in java & CPP

---
+++ Arrays/CPP/firstLastOccurence.cpp
@@ -0,0 +1,71 @@
+
+#include<iostream>
+#include<array>
+using namespace std;
+
+int firstOcurr(int arr[],int size, int key){
+    //Occurence of the key in the array (leftmost) 
+
+    int low = 0;
+    int high = size - 1;
+    int mid = low + (high - low)/2;  
+    int ans = 0;
+    while (low<=high)
+    {
+        if (arr[mid]==key)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid]<key)
+        {
+            low = mid + 1;
+        }
+
+        else if (arr[mid]>key)
+        {
+            high = mid - 1;
+        }
+        mid = low + (high - low)/2; 
+    }
+    return ans;
+}
+
+
+int lastOcurr(int arr[],int size, int key){
+    //Occurence of the key in the array (rightmost) 
+    
+    int low = 0;
+    int high = size - 1;
+    int mid = low + (high - low)/2;  
+    int ans = 0;
+    while (low<=high)
+    {
+        if (arr[mid]==key)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid]<key)
+        {
+            low = mid + 1;
+        }
+
+        else if (arr[mid]>key)
+        {
+            high = mid - 1;
+        }
+        mid = low + (high - low)/2; 
+    }
+    return ans;
+}
+
+
+int main(){
+    int arr[11] = {1,2,3,3,3,3,3,3,3,3,5};
+    int first = firstOcurr(arr,11,3);
+    int last = lastOcurr(arr,11,3);
+
+    cout<<"First occurence of the number is : "<<first<<endl;
+    cout<<"Last occurence of the number is : "<<last<<endl;
+}
---
+++ Arrays/CPP/move-ve.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <array>
+using namespace std;
+
+void sortArr(array <int,10> arr, int size){
+    int i = 0;
+    int j = size-1;
+    int mid = 0;
+    char check = 'P';
+    // Using one of my favourite Dutch national flag problem (Three pointer approach);
+    while (mid<=j)
+    {
+        if (arr[mid]<0){
+            check = 'N';
+        }
+        else
+        {
+            check = 'P'; 
+        }
+        
+        switch (check)
+        {
+        case 'P':
+            swap(arr[mid],arr[j]);
+            j--;
+            break;
+        
+        case 'N':
+            swap(arr[mid++],arr[i++]);
+            break;
+        
+        default:
+            cout<<"wrong\n";
+            break;
+        }
+    }
+    cout<<"Negatives after taking to one side: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
+
+int main(){
+    array<int,10>arr;
+    int size;
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    cout<<"Enter the values of array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    sortArr(arr,size);
+}
