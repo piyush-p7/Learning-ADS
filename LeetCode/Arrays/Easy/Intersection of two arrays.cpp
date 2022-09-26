fb66a030afa3be78f71b594f27c83a069dd85123
Piyush <piyushpandita151@gmail.com>
2022-09-27 01:27:43 +0530

    349

---
+++ LeetCode/Arrays/Easy/Intersection of two arrays.cpp
@@ -0,0 +1,43 @@
+// 349. Intersection of Two Arrays
+// Given two integer arrays nums1 and nums2, return an array of their intersection. 
+// Each element in the result must be unique and you may return the result in any order.
+// Example 1:
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+// Example 2:
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [9,4]
+// Explanation: [4,9] is also accepted.
+code below: 
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int n = nums1.size();
+        int m = nums2.size();
+        set<int> s;
+
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i] == nums2[j]){
+                s.insert(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        
+        //Using the set, as not to get duplicate values.
+        vector<int> ans;
+        for(auto i:s) ans.push_back(i);
+        return ans;
+  }
+};
