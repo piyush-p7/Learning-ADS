a89f414f70fd1edf23e6e3efeb5dac4d4ab966cd
Piyush <piyushpandita151@gmail.com>
2022-10-14 20:48:08 +0530

    Selection Sort

--- Arrays/CPP/Sorting Techniques/SelectionSort.cpp
+++ Arrays/CPP/Sorting Techniques/SelectionSort.cpp
@@ -1,49 +1,27 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void selection(vector<int> &arr)  
-{  
-    int i, j, small;  
-    int n = arr.size();
-      
-    for (i = 0; i < n-1; i++)    // One by one move boundary of unsorted subarray  
-    {  
-        small = i; //minimum element in unsorted array  
-          
-        for (j = i+1; j < n; j++)  
-        if (arr[j] < arr[small])  
-            small = j;  
-        // Swap the minimum element with the first element  
-        int temp = arr[small];  
-        arr[small] = arr[i];  
-        arr[i] = temp;  
-    }
+void printVec(vector<int> &arr){
     cout<<"After sorting the vector: ";
     for (int i = 0; i < arr.size(); i++)
     {
         cout<<arr[i]<<" ";
-    }  
-}  
+    } 
+}
+ 
 void selectionSort(vector<int> &vec){
     int n = vec.size();
     for (int i = 0; i < n-1; i++)
     {
         int minIndex = i;
-        for (int j = i+1; i < n; j++)
+        for (int j = i+1; j < n; j++)
         {
             if (vec[j]<vec[minIndex])
             {
                 minIndex = j;
             }
         }
-        int temp = vec[minIndex];  
-        vec[minIndex] = vec[i];  
-        vec[i] = temp; 
-    }
-    cout<<"After sorting the vector: ";
-    for (int i = 0; i < vec.size(); i++)
-    {
-        cout<<vec[i]<<" ";
+        swap(vec[i],vec[minIndex]);
     }
 }
 int main(){
@@ -58,6 +36,7 @@ int main(){
         cin >> value;
         vec.push_back(value);
     }
-    selection(vec);
+    selectionSort(vec);
+    printVec(vec);
     return 0;
 }
