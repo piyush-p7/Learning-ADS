601f8d0bc885f81404a0c3bcb9bb394ede7f73cf
Piyush <piyushpandita151@gmail.com>
2022-10-27 20:50:11 +0530

    Insertion Sort

---
+++ Arrays/CPP/Sorting Techniques/InsertionSort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void insertionSort(vector<int>& arr){
+    int i = 1;
+    while (i<arr.size())
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j>=0)
+        {
+            if (arr[j]>temp)
+            {
+                arr[j+1] = arr[j];
+                
+            }
+            else if(arr[j]<=temp){
+                break;
+            }
+            j--;
+        }
+        arr[j+1] = temp;
+        i++;
+    }
+    
+}
+void printArr(vector<int>&arr){
+    for(int i = 0; i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    vector<int> arr = {5,4,3,2,1,0};
+    insertionSort(arr);
+    printArr(arr);
+    return 0;
+}
