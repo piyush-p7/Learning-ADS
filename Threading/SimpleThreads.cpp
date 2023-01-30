6d2744eb200d4e94be48f5e6f144365d7ae8653d
Piyush <piyushpandita151@gmail.com>
2023-01-31 03:27:56 +0530

    Threading

--- Threading/SimpleThreads.cpp
+++ Threading/SimpleThreads.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <thread>
+#include<bits/stdc++.h>
+#include<unistd.h>
 using namespace std;
+
 void func1()
 {
-    std::cout << "Thread 1 is executing" << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        sleep(1);
+        std::cout << "Thread 1 is executing: " << i <<std::endl;
+        fflush(stdout);
+    }
+    
+}
+void func2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        sleep(1);
+        std::cout << "\nThread 2 is executing: " << i << std::endl;
+        fflush(stdout);
+    }      
 }
 
 int main()
 {
     thread t1(func1);
+    thread t2(func2);
+
     t1.join();
+    t2.join();
     return 0;
 }
