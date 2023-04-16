1cc5b963714eccf160697a980e793a308b4afdcc
Piyush <piyushpandita151@gmail.com>
2023-04-16 16:13:46 +0530

    NameDividing

---
+++ Strings/nameDivider.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void nameDivide(string name){
+    string firstName, middleName, lastName;
+    int i=0;
+    for(i=0;i<name.size();i++){
+        if(name[i] == '_'){
+            break;
+        }
+        firstName+=name[i];
+    }
+    cout<<"First Name: "<<firstName;
+    for(i++;i<name.size();i++){
+        if(name[i] == '_'){
+            break;
+        }
+        middleName+=name[i];
+    }
+
+    if(i>=name.size()){
+        string temp = middleName;
+        middleName = lastName;
+        lastName = temp;
+    }
+    else{
+        for(i++;i<name.size();i++){
+            lastName+=name[i];
+        }
+    }
+    cout<<"\nMiddle Name: "<<middleName;
+    cout<<"\nLast Name: "<<lastName;
+}
+int main() {
+    string fullName = "";
+    cout<<"Enter the full name: ";
+    cin>>fullName;
+    nameDivide(fullName);
+    return 0;
+}
